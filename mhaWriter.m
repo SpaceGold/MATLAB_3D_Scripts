62ef000d3a31c039a448f9ada5f0a4c3f739405f
Space Gold Studios - by Adam Zimmerman <amc3927@yahoo.com>
2020-04-01 12:42:09 -0700

    Merge functions work
    This is the first stable build of the mha label merger script. It requires a .csv of label tuples to combine, organized by column for each timepoint.

---
+++ mhaWriter.m
@@ -0,0 +1,60 @@
+function mhaWriter(filename, img, resolution, data_type)
+
+[pathstr,name, ext]=fileparts(filename);
+fid=fopen(filename, 'w');
+if(fid<=0)
+    printf('Impossible to open file %s\n', filename);
+end
+
+s = size(img)
+
+if numel(s) == 2
+    fprintf(fid, 'NDims = 2\n'); % Change this to the number of dimensions in your data set
+    fprintf(fid, 'DimSize = %d %d\n', size(img,1), size(img,2) );%should reflect number of dimensions
+    if(strcmp(data_type, 'char') || strcmp(data_type, 'uint8'))
+        fprintf(fid, 'ElementType = MET_UCHAR\n');
+    elseif(strcmp(data_type, 'short'))
+        fprintf(fid, 'ElementType = MET_SHORT\n');
+    elseif(strcmp(data_type, 'float'))
+        fprintf(fid, 'ElementType = MET_FLOAT\n');
+    elseif(strcmp(data_type, 'double'))
+        fprintf(fid, 'ElementType = MET_DOUBLE\n');
+    end
+    fprintf(fid, 'ElementSpacing = %1.4f %1.4f\n', resolution(1), resolution(2) ); % should reflect number of dimensions
+else
+    fprintf(fid, 'NDims = 3\n'); % Change this to the number of dimensions in your data set
+    fprintf(fid, 'DimSize = %d %d %d\n', size(img,1), size(img,2), size(img,3));%should reflect number of dimensions
+    if(strcmp(data_type, 'char') || strcmp(data_type, 'uint8'))
+        fprintf(fid, 'ElementType = MET_UCHAR\n');
+    elseif(strcmp(data_type, 'short'))
+        fprintf(fid, 'ElementType = MET_SHORT\n');
+    elseif(strcmp(data_type, 'float'))
+        fprintf(fid, 'ElementType = MET_FLOAT\n');
+    elseif(strcmp(data_type, 'double'))
+        fprintf(fid, 'ElementType = MET_DOUBLE\n');
+    end
+    fprintf(fid, 'ElementSpacing = %1.4f %1.4f %1.4f\n', resolution(1), resolution(2), resolution(3));
+end
+
+
+fprintf(fid, 'ElementByteOrderMSB = False\n');
+
+if isequal(ext,'.mhd')
+    fprintf(fid, 'ElementDataFile = %s\n', strcat(name, '.raw')); % Actual data file. File can have any extension
+    if isempty(pathstr)
+        img_filename = strcat(name, '.raw');
+    else
+        img_filename = strcat(pathstr, '/', name, '.raw');
+    end
+    fid2=fopen( img_filename, 'w')
+    if(fid2<=0)
+        %printf('Impossible to open file %s\n', img_filename);
+    end
+    fwrite(fid2, img, data_type);
+    fclose(fid2);
+
+else
+    fprintf(fid, 'ElementDataFile = LOCAL\n'); % Actual data file. File can have any extension
+    fwrite(fid, img, data_type);
+end
+fclose(fid);
---
+++ testX.m
@@ -0,0 +1,3 @@
+function varargout = testX(varargin)
+ varargout = varargin * 19 * 21398283;
+end
