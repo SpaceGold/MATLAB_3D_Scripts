ff84b2f7bb5bcc456dfad17e3cfdc1151067a586
Space Gold Studios - by Adam Zimmerman <amc3927@yahoo.com>
2020-04-17 07:26:42 -0700

    new

--- AZ_label_splitter.m
+++ AZ_label_splitter.m
@@ -1,18 +1,25 @@
-% divide mha label sets into individual, cropped, blob-segmented ROIs/masks
+function AZ_label_splitter (varargin)
+% Divide mha label sets into individual, cropped, blob-segmented ROIs/masks
 % Adam Zimmerman 2020
 
-% To-do:
-% make thresholding and erosion bulletproof or at least user-optimizable
-% make script into function(s)/modularize
-% generalize even more
-% add interpolate:
-    % [ny,nx,nz] = size(mask);
-    % [y,x,z] = ndgrid(1:ny,1:nx,1:nz);
-    % [Y,X,Z] = ndgrid(1:ny,1:nx,1:1/zAniso:nz);
-    % imask = interp3(x,y,z,double(mask),X,Y,Z,'nearest');
-clear global
-clearvars
-clc  % raw data file management:
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('rt', @ischar); % root for new dir and masks
+ip.addParameter('AnalysisPath', 'Masks', @ischar); % new dir
+ip.addRequired('fn_crop', @ischar); % cropped raw data in
+ip.addParameter('thresh', 48, @isnumeric); % threshold
+ip.addParameter('dKr', 1, @isnumeric); % dilation kernel radius for gaps
+ip.addParameter('eKr', 1, @isnumeric); % erosion kernel radius for gaps
+ip.addParameter('conn', 18, @isnumeric); % conn mode: 6, 18, or 26
+ip.addParameter('minCCsize', 1500, @isnumeric); % conn comp size in voxels
+ip.addParameter('save open', 1, @isboolean); % save open mask?
+ip.addParameter('save closed', 1, @isboolean); % save closed mask?
+
+p = ip.Results;
+mkdir([rt filesep p.AnalysisPath]);
+
+
+% raw data file management:
 cd 'C:\Users\amc39\Google Drive\ABC\Vignesh_SP\'; 
 rawFds = fileDatastore('Raw*', 'ReadFcn', @importdata);
 rawFullNames = rawFds.Files; % get raw names, e.g. Raw_ROI1.mha, Raw_ROI2.mha
@@ -76,60 +83,8 @@ for set = 1:nSet % split label sets
         croppedMask = rawSmall .* logical(croppedMask); % combine
         toc
         tic
-        % threshold - not lining up properly
-        %smallMask(smallMask<thresh) = 0;
-        %smallMask =logical(smallMask);
-%         
-% %                % erode
-%         CC = bwconncomp(croppedMask, 26); %survey the volume for connected components
-%         % discard small components (assumed to be noise or debris on glass slide)
-%         csize = cellfun(@numel, CC.PixelIdxList); % size of all objects in voxels
-%         idx = csize>=500; % <<<<<<<<<<<<<<<<<<< EXPERIMENT WITH THIS
-%         CC.NumObjects = sum(idx);
-%         CC.PixelIdxList = CC.PixelIdxList(idx);
-%         mask = croppedMask(CC)~=0;
-%         mask = imerode(mask, eKr); % erode
-%         newim_2 = zeros(size(mask),'uint8'); % reinitialize raw data
-%         newim_2(mask) = im(mask); % set everything outside the mask to zero
-%         tic
-%         fn_newim_2 = [rt filesep p.AnalysisPath filesep 'segCh3_' 'ekr' num2str(p.ekr) '_dkrbb' num2str(p.dkr_bb) '_dkrc' num2str(p.dkr_c) '_min_P_bb' num2str(p.min_P_bb) '_max_p_bk' num2str(p.max_p_bk) '_minBBsize' num2str(p.minBBsize) '_' fn_probabilities];
-%         % writetiff(newim_2, fn_newim_2);
-%  
-%        
-        %suffix = ['se_' dKr '_label_' label]; % buggy
-        mhaWriter(['test1_', name, sprintf('_label_%d',label), ext], croppedMask, [1,1,1], 'uint8'); % save
+        mhaWriter(['crop_', name, sprintf('_label%d',label), ext], croppedMask, [1,1,1], 'uint8'); % save
         toc          
     end
 end
-
-%%
-    
-%     % if cropping and eroding existing files
-% fds2 = fileDatastore('test*', 'ReadFcn', @importdata); % grab all label sets
-% fullFileNames = fds2.Files; % get set names, e.g. ROI1.mha, ROI2.mha...
-% blobCount = size(fullFileNames, 1); % loop through several sets
-% se2 = strel('sphere',3)
-
-%     for blob = 1:blobCount % Count % NOTE ME <<<<<<<<<<<<<<<<<
-%         fullName = fullFileNames(blob);
-%         [rt, name, ext] = fileparts(fullName{1}); % set name and ext
-%         [volume,vol_info] = ReadData3D(fullFileNames{blob}); % import volume data
-%         report = sprintf('%d labels found in %s', blobCount, rt); disp(report);
-%     end
-    %     % erode
-    %     CC = bwconncomp(Vout, 26); %survey the volume for connected components
-    %     % discard small components (assumed to be noise or debris on glass slide)
-    %     csize = cellfun(@numel, CC.PixelIdxList); % size of all objects in voxels
-    %     idx = csize>=500; % <<<<<<<<<<<<<<<<<<< EXPERIMENT WITH THIS
-    %     CC.NumObjects = sum(idx);
-    %     CC.PixelIdxList = CC.PixelIdxList(idx);
-    %     mask = labelmatrix(CC)~=0;
-    %     mask = imdilate(mask, se2); % dilate to connect disconnected objects from earlier erosion
-    %     newim_2 = zeros(size(im),'uint8'); % reinitialize raw data
-    %     newim_2(mask) = im(mask); % set everything outside the mask to zero
-    %     tic
-    %     fn_newim_2 = [rt filesep p.AnalysisPath filesep 'segCh3_' 'ekr' num2str(p.ekr) '_dkrbb' num2str(p.dkr_bb) '_dkrc' num2str(p.dkr_c) '_min_P_bb' num2str(p.min_P_bb) '_max_p_bk' num2str(p.max_p_bk) '_minBBsize' num2str(p.minBBsize) '_' fn_probabilities];
-    %     % writetiff(newim_2, fn_newim_2);
-    %     mhaWriter(['test_erode_', name, sprintf('_%d',blob), ext], Vout, [1,1,1], 'uint8'); % save
-    %     toc
-    % end 
+end
---
+++ AZ_mask_manager.m
@@ -0,0 +1,8 @@
+% Takes in sets of combined labels, separates them, crops new ROIs,
+% and customizes masks, open or closed. Manages AZ_label_splitter and
+% AZ_threshold_cleanup.m
+% Adam Zimmerman 2020
+clear global
+clearvars
+clc  
+
